c335b99eb1c6fdcac9d616de71e7abce3ac6aa3f
Andrea Bizzotto <bizz84@gmail.com>
2021-03-26 09:06:27 +0000

    Update AnimatedPageFlipBuilder to extend AnimatedWidget for less boilerplate code

--- lib/page_flip_builder.dart
+++ lib/page_flip_builder.dart
@@ -163,24 +163,25 @@ class PageFlipBuilderState extends State<PageFlipBuilder>
   }
 }
 
-class AnimatedPageFlipBuilder extends StatelessWidget {
+class AnimatedPageFlipBuilder extends AnimatedWidget {
   const AnimatedPageFlipBuilder({
     Key? key,
-    required this.animation,
+    required Animation<double> animation,
     required this.showFrontSide,
     required this.frontBuilder,
     required this.backBuilder,
     this.flipAxis = Axis.horizontal,
     this.maxTilt = 0.003,
     this.maxScale = 0.2,
-  }) : super(key: key);
-  final Animation<double> animation;
+  }) : super(key: key, listenable: animation);
   final bool showFrontSide;
   final WidgetBuilder frontBuilder;
   final WidgetBuilder backBuilder;
   final Axis flipAxis;
   final double maxTilt;
   final double maxScale;
+
+  Animation<double> get animation => listenable as Animation<double>;
 
   bool get _isAnimationFirstHalf => animation.value.abs() < 0.5;
 
@@ -210,22 +211,17 @@ class AnimatedPageFlipBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animation,
-      builder: (context, _) {
-        final child = _isAnimationFirstHalf ^ showFrontSide
-            ? backBuilder(context)
-            : frontBuilder(context);
-        final matrix = flipAxis == Axis.horizontal
-            ? (Matrix4.rotationY(_rotationAngle())..setEntry(3, 0, _getTilt()))
-            : (Matrix4.rotationX(_rotationAngle())..setEntry(3, 1, _getTilt()));
-        final scale = _scale();
-        return Transform(
-          transform: matrix * Matrix4.diagonal3Values(scale, scale, 1.0),
-          child: child,
-          alignment: Alignment.center,
-        );
-      },
+    final child = _isAnimationFirstHalf ^ showFrontSide
+        ? backBuilder(context)
+        : frontBuilder(context);
+    final matrix = flipAxis == Axis.horizontal
+        ? (Matrix4.rotationY(_rotationAngle())..setEntry(3, 0, _getTilt()))
+        : (Matrix4.rotationX(_rotationAngle())..setEntry(3, 1, _getTilt()));
+    final scale = _scale();
+    return Transform(
+      transform: matrix * Matrix4.diagonal3Values(scale, scale, 1.0),
+      child: child,
+      alignment: Alignment.center,
     );
   }
 }
